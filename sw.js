497f25c6528189f7948e0115173d4eac2cfa0c4d
sledochera060984-dotcom <sledochera060984@gmail.com>
2026-04-07 23:17:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
+const CACHE_NAME = 'arabus-v2';
+const assets = [
+  './',
+  './index.html',
+  './base.js'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      // Кэшируем файлы. base.js (8мб) может занять время.
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
