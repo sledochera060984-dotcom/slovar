1a5b1f14976e301c003e55d420d1a8cd0be69a65
sledochera060984-dotcom <sledochera060984@gmail.com>
2026-04-08 07:37:10 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,10 @@
-const CACHE_NAME = 'arabus-v2';
-const assets = [
-  './',
-  './index.html',
-  './base.js'
-];
+const CACHE = 'arabus-v2';
+const FILES = ['./', './index.html', './base.js', './manifest.json'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      // Кэшируем файлы. base.js (8мб) может занять время.
-      return cache.addAll(assets);
-    })
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
